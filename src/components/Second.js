041e08e4133825446f7bf449c8cdd4d704c08363
endrithashani <94977365+endrithashani@users.noreply.github.com>
2022-03-17 18:56:09 +0100

    Add files via upload

--- src/components/Second.js
+++ src/components/Second.js
@@ -16,13 +16,61 @@ const Card= [
         color:"#0000FF",
     },
     {
-            image:"./images/profile.png",
-            title:"3.1.0 (20/05/2015)",
-            button:"Improvement",
-            name:"Kevin Joe",
-            description:"Added Settings for Auto start Added Update Notification  Speed Optimization  Bug fixes " ,
-            color:"#8A2BE2",
-        }
+        image:"./images/profile.png",
+        title:"3.1.0 (20/05/2015)",
+        button:"Improvement",
+        name:"Kevin Joe",
+        description:"Added Settings for Auto start Added Update Notification  Speed Optimization  Bug fixes " ,
+        color:"#8A2BE2",
+    },
+    {
+        image:"./images/profile.png",
+        title:"3.3.0 (14/05/2018)",
+        button:"New",
+        name:"Kevin Joe",
+        description:"File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvements Share premissions re-design and optimization ",
+        color:"#008000",
+    },
+    {
+        image:"./images/profile.png",
+        title:"3.1.0 (14/05/2018)",
+        button:"Fix",
+        name:"Kevin Joe",
+        description:"Introducing Host Cloud Drive-virtual drive functionality New Share options and management New , more user friendly design Sync optimizations Various performance improvments and bug fixed",
+        color:"#0000FF",
+    },
+    {
+        image:"./images/profile.png",
+        title:"3.1.0 (20/05/2015)",
+        button:"Improvement",
+        name:"Kevin Joe",
+        description:"Added Settings for Auto start Added Update Notification  Speed Optimization  Bug fixes " ,
+        color:"#8A2BE2",
+    },
+    {
+        image:"./images/profile.png",
+        title:"3.3.0 (14/05/2018)",
+        button:"New",
+        name:"Kevin Joe",
+        description:"File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvements Share premissions re-design and optimization ",
+        color:"#008000",
+    },
+    {
+        image:"./images/profile.png",
+        title:"3.1.0 (14/05/2018)",
+        button:"Fix",
+        name:"Kevin Joe",
+        description:"Introducing Host Cloud Drive-virtual drive functionality New Share options and management New , more user friendly design Sync optimizations Various performance improvments and bug fixed",
+        color:"#0000FF",
+    },
+    {
+        image:"./images/profile.png",
+        title:"3.1.0 (20/05/2015)",
+        button:"Improvement",
+        name:"Kevin Joe",
+        description:"Added Settings for Auto start Added Update Notification  Speed Optimization  Bug fixes " ,
+        color:"#8A2BE2",
+    },
 ]
 const myStyle={
     width:'30px',
@@ -30,7 +78,7 @@ const myStyle={
     };
 const Second = (props) => {
     return ( 
-        <div className="container">
+        <div className="container-second">
         {Card.map((props) =>{
             const{image , title,button, name ,description,color} = props;
             return (
